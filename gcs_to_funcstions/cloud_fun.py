ceec7099dec971e54ffd441c74914de8d21f3c3f
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2022-04-22 19:09:29 +0530

    added: gcs_to_fucntions

---
+++ gcs_to_funcstions/cloud_fun.py
@@ -0,0 +1,35 @@
+from google.cloud import storage
+import pandas as pd
+
+
+source_bucket_name = 'bucket_name'
+source_blob_name = 'data.xlsx'
+destination_bucket_name = 'bucket_name'
+destination_blob_name = 'data.csv'
+
+
+
+def convert_to_csv(source_bucket_name, source_blob_name,destination_bucket_name,destination_blob_name):
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(source_bucket_name)
+    #bucket.list_blobs()
+    blob = bucket.blob(source_blob_name)
+    contents = blob.download_as_string()
+    xl = pd.read_excel(contents)
+    xl.to_csv("data.csv")
+    
+    dest_bucket = storage_client.bucket(destination_bucket_name)
+    blobb = dest_bucket.blob(destination_blob_name)
+
+    blobb.upload_from_filename("data.csv")
+
+
+    print('CSV file uploaded to bucket')
+
+
+convert_to_csv(source_bucket_name, source_blob_name,destination_bucket_name,destination_blob_name)
+
+
+
